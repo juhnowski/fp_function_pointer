ad2c460a69372156969341c0b8d51b73ad92cea6
Juhnowski <juhnowski@gmail.com>
2020-08-01 04:35:53 +0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+#  Function pointers
+
+This example demonstrates that you can create a function pointer (ask_ptr) that points to an ordinary function, and a function reference (ask_ref) that references the same function, and that you can call them as if they were functions themselves—with the usual function call syntax.
--- fp_func_pointer/main.cpp
+++ fp_func_pointer/main.cpp
@@ -1,15 +1,25 @@
-//
-//  main.cpp
-//  fp_func_pointer
-//
-//  Created by Илья Юхновский on 01.08.2020.
-//  Copyright © 2020 Илья Юхновский. All rights reserved.
-//
-
 #include <iostream>
 
+int ask() {return 42;}
+
+typedef decltype(ask)* function_ptr;
+
+class convertible_to_function_ptr {
+public:
+    operator function_ptr() const {
+        return ask;
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    auto ask_ptr = &ask;
+    std::cout << ask_ptr() << std::endl;
+    
+    auto& ask_ref = ask;
+    std::cout << ask_ref() << std::endl;
+    
+    convertible_to_function_ptr ask_wrapper;
+    std::cout << ask_wrapper() << std::endl;
+    
     return 0;
 }
